bab1dedfc04b3a7058a8a04d00f347d590d02b99
Jesus Marquez <marquezm2410@gmail.com>
2022-05-26 23:38:31 -0400

    PR 2

--- test/route.spec.js
+++ test/route.spec.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 describe('DESCRIBE (Probando rutas)', () => {
   describe('DESCRIBE (Probando GET notas', () => {
     test('Existe ruta', async () => {
-      const envio = await request(app).get('/notass').send();
+      const envio = await request(app).get('/notas').send();
       expect(envio.status).toBe(200);
     });
 
